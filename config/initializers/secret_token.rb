7dee14ff17b7826c42b0a4a3c1c7ee66ce09be22
Neil Sethi <neilsethi@gmail.com>
2014-01-18 09:35:31 -0500

    Added app lol

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horchata::Application.config.secret_token = '4580ead062899fb304ad3d380cdac0daf9ecdc803bd8e262186109bff534fe2ea9097d21c653ac036353f8a4616e54777d21d7305b9ac0472782829e26a5c13c'
