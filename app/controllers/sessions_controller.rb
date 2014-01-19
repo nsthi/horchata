fa407c069b2973fa2a777e3076e8d713d354b62f
Neil Sethi <neilsethi@gmail.com>
2014-01-19 03:51:58 -0500

    Fixed some bullshit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,25 +5,11 @@ class SessionsController < ApplicationController
     redirect_to root_url
     @graph = Koala::Facebook::API.new(user.oauth_token)
     
-    print 'lol'
     pictures = @graph.fql_query("SELECT pid, src_big, like_info FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject = me()) AND pid IN (SELECT pid FROM photo_tag WHERE subject = 1128630265)")
 	pictures = pictures.sort_by{|k| k['like_info']['like_count']}
 	pictures.each do |h|
 		print h['like_info']['like_count'].to_int
 	end
-	#print "LOLOLOL"
-	#query = "SELECT pid, src_big FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject = me()) AND pid IN (SELECT pid FROM photo_tag WHERE subject = 100000946344212)"
-	#friends = @graph.fql_query(query)
-
-	#print lolwut
-	# print lolwut
-	#print friends
-	#friends = @graph.get_connections("me", "friends")
-	#friends.each do |f|
-	#   f[:image]= @graph.get_picture(f["id"], :type=>'large')
-	#   print f[:image]
-	#end
-
   end
 
   def destroy
